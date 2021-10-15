4963bf434805e10eb400994ed97d9f367b20ed0b
tamdrav333 <migration.service.ra@gmail.com>
2021-10-15 16:56:21 +0400

    migration AND htaccess

--- aaaaMigrations.php
+++ aaaaMigrations.php
@@ -2,6 +2,8 @@ ALTER TABLE tb_translate ADD COLUMN  translate_date DATETIME;
 ALTER TABLE tb_translate ADD COLUMN  translate_time_from VARCHAR(50);
 ALTER TABLE tb_translate ADD COLUMN  translate_time_to VARCHAR(50);
 
+
+
 CREATE TABLE IF NOT EXISTS cover_sign_status (
 sign_status_id INT AUTO_INCREMENT PRIMARY KEY,
 sign_status_name VARCHAR(255) NOT NULL
@@ -20,6 +22,20 @@ VALUES
 ('Ուղարկվել է հաստատման'),
 ('Ուղարկվել է թարգմանության'),
 ('Ստացվել է պատասխան');
+
+INSERT INTO tb_file_type (file_type, file_filter)
+VALUES
+('Մկրտության վկայական', '2'),
+('Վարորդական վկայական', '2'),
+('Անձնագրի թարգմանություն', '2'),
+('Ներքին անձնագիր', '1'),
+('Քրեական գործ', '1'),
+('Կրթության վկայական', '2');
+
+
+
+
+
 
 
 ALTER TABLE tb_translate ADD COLUMN  sign_status INT(1);
